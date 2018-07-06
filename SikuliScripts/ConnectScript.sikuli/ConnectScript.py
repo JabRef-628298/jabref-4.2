ff696196197ad54a4fe5560c6b0d98019afb92f8
Tiago Bachiega de Almeida <tiago_almeida0297@hotmail.com>
2018-07-06 14:57:44 -0300

    Adicionados scripts da Sikuli
    Adicionados os seguintes scripts:
    - Connection
    - Manual Connect
    - Select Style
    - Settings
    
    Arquivos HTML, Python e imagens necessárias.

---
+++ SikuliScripts/ConnectScript.sikuli/ConnectScript.py
@@ -0,0 +1,21 @@
+App.open("java -jar C:/JabRef-4.2-fat.jar")
+wait(30)
+click("1529632189350.png")
+wait(2)
+click("1529632296782.png")
+wait(2)
+click("1529632312432.png")
+wait(2)
+click("1529632554435.png")
+wait(2)
+click("1529632574104.png")
+wait(2)
+click("1529632589427.png")
+wait(2)
+click("1529632601277.png")
+wait(2)
+click("1529632757740.png")
+
+
+
+
